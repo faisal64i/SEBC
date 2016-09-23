99517e81382a7a78ac17fade1eaeb5786c54eb03
Faisal Zaman <faisal64i@gmail.com>
2016-09-23 11:14:03 -0400

    updated

---
+++ challenges/labs/2_cm.d
@@ -0,0 +1,11 @@
+
+
+can't find:  /etc/yum/repos.d
+
+====
+
+[root@ip-172-31-8-189 ec2-user]# ls /etc/yum/
+fssnap.d  pluginconf.d  protected.d  vars  version-groups.conf  yum-cron.conf  yum-cron-hourly.conf
+[root@ip-172-31-8-189 ec2-user]#
+
+===
