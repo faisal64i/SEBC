e2f0de74e2a471683ce3dd400f7a5b69c84abe3d
Faisal Zaman <faisal64i@gmail.com>
2016-09-23 12:03:10 -0400

    done

--- challenges/labs/2_cm.d
+++ challenges/labs/2_cm.d
@@ -9,3 +9,24 @@ fssnap.d  pluginconf.d  protected.d  vars  version-groups.conf  yum-cron.conf  y
 [root@ip-172-31-8-189 ec2-user]#
 
 ===
+
+CREATE USER
+'cdhuser'@'localhost' IDENTIFIED BY 'Cdh!2password'
+PASSWORD EXPIRE NEVER
+;
+
+GRANT ALL on *.* TO 'cdhuser'@'%' IDENTIFIED BY 'Cdh!2password';
+
+====
+
+
+head: cannot open ‘/var/log/cloudera-scm-server/cloudera-scm-server.log’ for reading: No such file or directory
+
+
+====
+
+
+grep: /var/log/cloudera-scm-server/cloudera-scm-server.log: No such file or directory
+
+
+===
